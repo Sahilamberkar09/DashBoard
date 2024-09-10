6c8100cbf5b4cd1da465ed5637e64e70a75117ce
Sahil Amberkar <sahilamberkar2003@gmail.com>
2024-09-10 17:17:07 +0530

    Files Updates

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".menu > ul > li").click(function (e) {
+  $(this).siblings().removeClass("active");
+  $(this).toggleClass("active");
+
+  $(this).find("ul").slideToggle();
+
+  $(this).siblings().find("ul").slideUp();
+
+  $(this).siblings().find("li").removeClass("active");
+});
